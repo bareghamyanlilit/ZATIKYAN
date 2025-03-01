f6229a7566c2cb9bccd7488d3ff4bc8cd38fb11c
avetisyanmarine <marineavet2006@gmail.com>
2025-03-01 16:45:37 +0400

    footer-part

--- src/component/footer/index.jsx
+++ src/component/footer/index.jsx
@@ -1,7 +1,78 @@
-import { FooterDiv } from './styled'
+import React from "react"
+import { FooterContainer, FooterSection, FooterTitle, FooterColumn, FooterFlexible, FooterLinks } from './styled';
+import { Link } from 'react-router-dom';
+import { FaEnvelope, FaFacebook, FaInstagram, FaPhoneSquareAlt } from 'react-icons/fa'
+import { Container, Flexible } from "../../styled";
+import Logo from "../../assets/img/logo.png";
+import { DiVisualstudio } from "react-icons/di";
 
-export const MyFooter=()=>{
-	return(
-		<FooterDiv></FooterDiv>
+export const MyFooter = () => {
+	return (
+		<FooterContainer>
+			<Container>
+				<FooterFlexible>
+						<img src={Logo} alt="Zatikyan Physical Therapy" />
+					<FooterColumn>
+						<FooterTitle>Our Adress</FooterTitle>
+						<Link to="" >Avag Petrosyan 4, Yerevan</Link>
+					</FooterColumn>
+					<FooterColumn>
+						<FooterTitle>Working Hours</FooterTitle>
+						<Flexible>
+							<div>
+								<p>Monday</p>
+								<p>Tuesday</p>
+								<p>Wednesday</p>
+								<p>Thursday</p>
+								<p>Friday</p>
+								<p>Saturday</p>
+								<p>Sunday</p>
+							</div>
+							<div>
+								<p>09:00 - 20:00</p>
+								<p>09:00 - 20:00</p>
+								<p>09:00 - 20:00</p>
+								<p>09:00 - 20:00</p>
+								<p>09:00 - 20:00</p>
+								<p>09:00 - 20:00</p>
+								<p>09:00 - 20:00</p>
+							</div>
+						</Flexible>
+					</FooterColumn>
+				</FooterFlexible>
+				<div>
+					<FooterSection>
+						<FooterTitle>Zatikyan Physical Therapy</FooterTitle>
+						<p>Improving your health with expert care and personalized treatment</p>
+					</FooterSection>
+
+					<FooterSection>
+						<FooterTitle>Quick Links</FooterTitle>
+						<FooterLinks>
+							<Link to="/">Home</Link>
+							<Link to="/services">Services</Link>
+							<Link to="/about">About Us</Link>
+							<Link to="/contact">Contact</Link>
+						</FooterLinks>
+					</FooterSection>
+
+					<FooterSection>
+						<FooterTitle>Follow Us</FooterTitle>
+						<FooterSection>
+							<Link to=""> <FaFacebook /> <p>Lorem ipsum dolor</p> </Link>
+							<Link to="https://www.instagram.com/zatikyan_physical_therapy/"> <FaInstagram /> <p>zatikyan_physical_therapy</p> </Link>
+						</FooterSection>
+					</FooterSection>
+					<FooterSection>
+						<FooterTitle>Contact with Us</FooterTitle>
+						<FooterSection>
+							<Link to="tel:+37495801851"> <FaPhoneSquareAlt /> <span>+374 95 80 18 51</span></Link>
+							<Link to="mailto:marineavet2006@gmail"> <FaEnvelope /> clincicemail@gmail.com </Link>
+						</FooterSection>
+					</FooterSection>
+					<p>&copy; 2025 Zatikyan Physical Therapy. All rights reserved.</p>
+				</div>
+			</Container>
+		</FooterContainer>
 	)
 }
--- src/component/footer/styled.jsx
+++ src/component/footer/styled.jsx
@@ -1,6 +1,75 @@
-import styled from "styled-components"
+import styled from 'styled-components';
+import { globalColor } from '../../assets/colors';
+import { Flexible } from '../../styled';
 
-export const FooterDiv = styled.div`
-	height:200px;
-	background: #565656;
+const FooterContainer = styled.footer`
+  background: ${globalColor.gray};
+  color: ${globalColor.white};
+  padding: 40px 20px;
+  text-align: center;
+`;
+const FooterColumn = styled(Flexible)`
+	align-items: start;
+	flex-direction: column;
+	// justify-content: space-around;
+	margin-bottom: 20px;
+	div {
+		text-align: left;
+	}
+	a {
+		color: ${globalColor.white};
+        margin-bottom: 5px;
+	}
 `
+
+const FooterSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	text-align: left;
+    gap: 5px;
+    color: ${globalColor.white};
+    text-decoration: none; 
+    &:hover {
+      color: ${globalColor.darkGray};
+      text-decoration: underline;
+    }
+    @media (max-width: 230px) {
+        p {
+            display: none;
+            color: red;
+        }
+    }
+  }
+`;
+
+const FooterTitle = styled.h4`
+  font-size: 20px;
+  color: ${globalColor.darkGray};
+`;
+
+const FooterLinks = styled.div`
+    a {
+        display: block;
+        color: ${globalColor.white};
+        text-decoration: none;
+        margin-bottom: 5px;      
+    }
+`;
+
+const FooterFlexible = styled.div`
+	display:flex;
+	justify-content: space-around;
+	align-items: center;
+	gap: 17px;
+	flex-wrap: wrap;
+`;
+
+export { FooterContainer, FooterFlexible, FooterSection, FooterColumn, FooterTitle, FooterLinks };
