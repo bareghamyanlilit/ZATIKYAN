0b10333f1ec8945e81dc017540e9e2730f2883a5
avetisyanmarine <marineavet2006@gmail.com>
2025-03-02 17:39:16 +0400

    Resolved merge conflict

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AboutPage, ContactPage, HomePage } from './container'
 import { BackToTop, MyFooter, MyHeader } from './component'
 import { GlobalStyles } from './styled'
-<<<<<<< HEAD
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import translationEN from './languages/en/translation.json'
-import translationRU from './languages/ru/translation.json'
-import translationAM from './languages/am/translation.json'
-export const MyProject = () => {
-	i18n.use(initReactI18next).init({
-=======
 import { initReactI18next } from 'react-i18next'
 import i18next from 'i18next'
 import translationEN from './languages/en/translation.json'
@@ -20,7 +11,6 @@ import translationAM from './languages/am/translation.json'
 
 export const MyProject=()=>{
 	i18next.use(initReactI18next).init({
->>>>>>> 3f731de4844732af5ac86b03a8842e34aa6ab917
 		resources: {
 			en: {
 				translation: translationEN,
