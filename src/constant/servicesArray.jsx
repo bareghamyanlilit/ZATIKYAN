92c6625b5fa90978f2660a74547f4911e4393b05
avetisyanmarine <marineavet2006@gmail.com>
2025-03-03 01:56:25 +0400

    Commit message describing your changes

--- src/constant/servicesArray.jsx
+++ src/constant/servicesArray.jsx
@@ -4,55 +4,48 @@ import img3 from '../assets/img/services/img3.jpg'
 import img4 from '../assets/img/services/img4.jpg'
 import img5 from '../assets/img/services/img5.jpg'
 import img6 from '../assets/img/services/img6.jpg'
+
 export const services = [
 	{
 		icon: '💆‍♂️',
-		title: 'Massage',
-		description:
-			'Relaxing and therapeutic massages with different techniques...',
-		details:
-			'Massage helps restore muscle strength, reduce tension and improve blood circulation.',
-		image:img5,
+		title: 'massage',
+		description: 'Relaxing and therapeutic massages with different techniques...',
+		details: 'Massage helps restore muscle strength, reduce tension and improve blood circulation.',
+		image: img5,
 	},
 	{
 		icon: '🧊',
-		title: 'Cryotherapy',
+		title: 'cryotherapy',
 		description: 'Cold therapy to reduce inflammation and pain...',
-		details:
-			'Cryotherapy helps in reducing muscle pain, swelling, and improves recovery time through controlled cold exposure.',
+		details: 'Cryotherapy helps in reducing muscle pain, swelling, and improves recovery time through controlled cold exposure.',
 		image: img1,
 	},
 	{
 		icon: '🌿',
-		title: 'Therapeutic Taping',
+		title: 'therapeuticTaping',
 		description: 'Support and stability for muscles and joints...',
-		details:
-			'Therapeutic taping helps reduce pain, improve circulation, and support injured muscles and joints without restricting movement.',
+		details: 'Therapeutic taping helps reduce pain, improve circulation, and support injured muscles and joints without restricting movement.',
 		image: img2,
 	},
 	{
 		icon: '👐',
-		title: 'Manual Therapy',
+		title: 'manualTherapy',
 		description: 'Hands-on techniques to improve movement and reduce pain...',
-		details:
-			'Manual therapy involves joint mobilization, soft tissue manipulation, and stretching techniques to enhance mobility and reduce discomfort.',
+		details: 'Manual therapy involves joint mobilization, soft tissue manipulation, and stretching techniques to enhance mobility and reduce discomfort.',
 		image: img4,
 	},
 	{
 		icon: '🏋️‍♀️',
-		title: 'Exercise therapy',
+		title: 'exerciseTherapy',
 		description: 'Personalized exercises for recovery...',
-		details:
-			'This therapy is designed to restore muscles and joints through special exercises.',
-		image:img3,
+		details: 'This therapy is designed to restore muscles and joints through special exercises.',
+		image: img3,
 	},
 	{
 		icon: '⚡',
-		title: 'Electrotherapy',
-		description:
-			'Electrical stimulation techniques that aid in neuromuscular recovery...',
-		details:
-			'Provides rapid recovery through electrical impulses that stimulate muscles.',
-		image:img6,
+		title: 'electrotherapy',
+		description: 'Electrical stimulation techniques that aid in neuromuscular recovery...',
+		details: 'Provides rapid recovery through electrical impulses that stimulate muscles.',
+		image: img6,
 	},
 ]
